1955f9b61e69fcfad66528dd1961e9ce917d48b6
NikaNastya <nika29052006@gmail.com>
2024-10-31 15:18:08 +0300

    Add files via upload

---
+++ пр13/пр13/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace пр13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "bD_Tur_FirmDataSet.Информация_о_туристах". При необходимости она может быть перемещена или удалена.
+            this.информация_о_туристахTableAdapter.Fill(this.bD_Tur_FirmDataSet.Информация_о_туристах);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "bD_Tur_FirmDataSet.Туристы". При необходимости она может быть перемещена или удалена.
+            this.туристыTableAdapter.Fill(this.bD_Tur_FirmDataSet.Туристы);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            туристыTableAdapter.Update(bD_Tur_FirmDataSet1);
+            информация_о_туристахTableAdapter.Update(bD_Tur_FirmDataSet1);
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
